a13196b3054fb2a45007a662e1579882879c8dbc
Peter Fesz-Nguyen <pfesznguyen@gmail.com>
2017-09-19 06:15:12 -0700

    use proxy for everything

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-var mixin = require('mixin-object');
-
 /*
  * @param {function} middleware - connect style, (req, res, next)
  * @return {function} middleware - dietjs style ($)
  */
 function compatible (middleware) {
 	return function ($) {
-		// clone $.request and mixin signal
-		var req = mixin(mixin({}, $.request), $);
+		// clone $ signal and mixin request
+		var req = $.request;
 		// proxy any changes to req to signal
 		var reqProxy = new Proxy(req, {
+			get: function (target, name) {
+				return name in target ? target[name] : $[name];
+			},
 			set: function (target, prop, value, receiver) {
 				target[prop] = value;
 				$[prop] = value;
